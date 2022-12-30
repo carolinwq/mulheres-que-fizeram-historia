e03d487a0a807dca145f85d262e2da67750ebb24
Carol Alambert <98852840+carolinwq@users.noreply.github.com>
2022-12-29 21:07:11 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+document.getElementById("botaoEnviar").addEventListener("click", validaFormulario)
+
+function validaFormulario(){
+  if(document.getElementById("nome").value != "" &&             document.getElementById("email").value != "" &&   document.getElementById("numero").value != ""){
+    alert("Prontinho! Você receberá as novidades por email.")
+  }else{
+    alert("Por favor, preencha os campos nome e email! :)")
+  }
+}
